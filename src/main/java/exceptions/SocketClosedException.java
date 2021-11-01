0bf81ec2df13df762d16b063e1a41d2f7ff5a3f5
sorinradneantu <radneantu_sorin@yahoo.com>
2021-11-01 20:53:14 +0200

    new tests added for ServerController

---
+++ src/main/java/exceptions/SocketClosedException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class SocketClosedException extends Exception{
+}
--- src/test/java/ServerControllerTest.java
+++ src/test/java/ServerControllerTest.java
@@ -1,3 +1,4 @@
+import exceptions.SocketClosedException;
 import exceptions.WrongServerException;
 import org.junit.Test;
 import webserver.ServerController;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -50,6 +52,105 @@ public class ServerControllerTest {
 
     }
 
+    @Test(expected = BindException.class)
+    public void testNewServerSocketPortNotAvailable() throws BindException {
+
+        ServerSocket serverSocket1 = ServerController.newServerSocket(port8080);
+        ServerSocket serverSocket2 = ServerController.newServerSocket(port8080);
+
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void closeServerSocketNotOk(){ // when try to close a null socket server
+
+        ServerController.closeServerSocket(null);
+
+    }
+
+
+    @Test
+    public void closeServerSocketOk(){
+
+        try{
+            ServerSocket serverSocket = ServerController.newServerSocket(port8080);
+            ServerController.closeServerSocket(serverSocket);
+            assertTrue(serverSocket.isClosed());
+        }catch(BindException e){
+            Assertions.fail(e);
+        }
+
+    }
+
+
+    @Test
+    public void testNewClientSocketOk() throws BindException {
+
+        ServerSocket serverSocket = ServerController.newServerSocket(port8080);
+
+        try{
+
+            Socket clientSocket = ServerController.newClientSocket(serverSocket);
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void testCloseClientSocketNotOk(){ // when try to close null client socket
+
+        ServerController.closeClientSocket(null);
+
+    }
+
+    @Test
+    public void testCloseClientSocketOk() throws BindException {
+
+        try{
+
+            ServerSocket serverSocket = ServerController.newServerSocket(port8080);
+            Socket clientSocket = ServerController.newClientSocket(serverSocket);
+
+            ServerController.closeClientSocket(clientSocket);
+            assertTrue(clientSocket.isClosed());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Test
+    public void testAcceptSocketOk() {
+        try {
+            ServerSocket serverSocket = ServerController.newServerSocket(port8080);
+            Socket clientSocket = ServerController.acceptSocket(serverSocket);
+
+            assertTrue(clientSocket.isBound());
+            assertTrue(serverSocket.isBound());
+
+            serverSocket.close();
+            clientSocket.close();
+        }catch(BindException e) {
+            Assertions.fail(e);
+        }catch(IOException e) {
+            Assertions.fail(e);
+        }catch(Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test(expected = SocketException.class)
+    public void testAcceptSocketNotOk() throws Exception { // when accept from a closed server socket
+        ServerSocket serverSocket = ServerController.newServerSocket(port8080);
+        ServerController.closeServerSocket(serverSocket);
+        Socket clientSocket = ServerController.acceptSocket(serverSocket);
+    }
+
 
     @Test
     public void clientHandlerOk() throws WrongServerException, IOException {
@@ -65,7 +166,7 @@ public class ServerControllerTest {
     }
 
     @Test
-    public void clientHandlerNotOk() throws WrongServerException {
+    public void clientHandlerNotOk() throws WrongServerException { // when try to handle a null client
         WebServer webServer = new WebServer(port8080,websiteFilePath,status.get(0));
         ServerController serverController = new ServerController(webServer);
 
@@ -108,8 +209,8 @@ public class ServerControllerTest {
 
         firstServerController.requestHandler();
 
-
     }
+
 
 
 }
